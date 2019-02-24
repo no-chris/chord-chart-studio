6082f615bc7f450271971adea1a91c864b253e6b
Christophe Noël <touffi51@gmail.com>
2019-02-24 17:06:31 +0100

    added more masks

--- tests/unit/createBarMask.spec.js
+++ tests/unit/createBarMask.spec.js
@@ -8,9 +8,11 @@ describe('createBarMask', () => {
 
 });
 
-describe.only.each([
+describe.each([
 	['1 bar / 1 chord  / 4 bpb', 			4, 'C', 			'{0}   ' ],
-	['1 bar / 2 chords / 4 bpb', 			4, 'C.. F..', 		'{0}   {1}' ],
+	['1 bar / 2 chords (1/3) / 4 bpb', 		4, 'C. F...', 		'{0} {1}     ' ],
+	['1 bar / 2 chords (2/2) / 4 bpb', 		4, 'C.. F..', 		'{0}   {1}   ' ],
+	['1 bar / 2 chords (3/1) / 4 bpb', 		4, 'C... F.', 		'{0}     {1} ' ],
 	['1 bar / 3 chords (1/1/2) / 4 bpb', 	4, 'C. F. G..', 	'{0} {1}   {2}' ],
 	['1 bar / 3 chords (2/1/1) / 4 bpb', 	4, 'C.. F. G.', 	'{0}   {1} {2}' ],
 	['1 bar / 4 chords / 4 bpb', 			4, 'C. F. G. Em.',	'{0}  {1}  {2}  {3}' ],
@@ -20,7 +22,7 @@ describe.only.each([
 	['1 bar / 2 chords (2/1) / 3 bpb', 		3, 'C.. F.',		'{0}      {1}' ],
 	['1 bar / 3 chords / 3 bpb', 			3, 'C. F. G.',		'{0}  {1}  {2}' ],
 
-	//['other', 								6, 'C.. F.. G..',	'{0}  {1}  {2}' ],
+	// @todo ['other', 								6, 'C.. F.. G..',	'{0}  {1}  {2}' ],
 ])('Mask for %s',
 	(title, beatsPerBar, input, output) => {
 		test('Correct mask is created', () => {
