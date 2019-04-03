c4df71a3c91ac64f07de1baf838201c34184942b
Christophe Noël <touffi51@gmail.com>
2019-04-03 22:12:42 +0200

    cover all core components with unit tests

---
+++ __mocks__/@touffi/ucc.js
@@ -0,0 +1,3 @@
+/* eslint-env jest */
+export const renderSong = jest.fn();
+export const parseSong = jest.fn();
--- src/core/createAction.js
+++ src/core/createAction.js
@@ -1,4 +1,7 @@
 export default function createAction(type, payload) {
+	if (!type) {
+		throw new TypeError('Cannot create an action without an action type');
+	}
 	return {
 		type,
 		payload
