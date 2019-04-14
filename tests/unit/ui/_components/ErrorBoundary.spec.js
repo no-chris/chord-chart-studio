a1bb21e6567d2898b5c21505e6d3fe7a271b7588
Christophe Noël <touffi51@gmail.com>
2019-04-14 22:58:17 +0200

    added ErrorBoundary and enable React StrictMode

---
+++ tests/unit/ui/_components/ErrorBoundary.spec.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import ErrorBoundary from '../../../../src/ui/_components/ErrorBoundary';
+
+afterEach(cleanup);
+
+describe('ErrorBoundary', () => {
+
+	test('Should render the error in case of an error in a child component', () => {
+		function Throw() {
+			throw new Error('This is bad');
+		}
+
+		// Don't do this at home!! Prevent the thrown error to be output in the console
+		/* eslint-disable no-console */
+		const consoleLogError = console.error;
+		console.error = () => {};
+
+		const { getByText } = render(
+			<ErrorBoundary>
+				<Throw />
+			</ErrorBoundary>
+		);
+
+		getByText('Something went wrong');
+		getByText(/This is bad/);
+
+		console.error = consoleLogError;
+		/* eslint-enable no-console */
+	});
+
+	test('Should display the children if no error', () => {
+		function DoNotThrow() {
+			return <div>This is good</div>;
+		}
+
+		const { getByText, queryByText } = render(
+			<ErrorBoundary>
+				<DoNotThrow />
+			</ErrorBoundary>
+		);
+
+		expect(queryByText('Something went wrong')).toBeNull();
+		getByText('This is good');
+	});
+});
--- todo.md
+++ todo.md
@@ -1,6 +1,4 @@
 # Tech debt
-- react strict mode
-- react error boundaries
 - remove handlebars (but depends on UCC...)
 - host React on CDN
 
@@ -9,13 +7,6 @@
 
 - CSS: decide what to do with the font size mixin
 - CSS: better abstract color variables names (foreground/background, remove systematic theme prefix)
-
-+ icon:    vertical-align: middle;
-+ check structure/skin/layout
-+ better encapsulation for icons in tests? Well anyway it will break on lib change because icons names are differents...
-+ remove old fileManager
-+ SCSS individual imports
-+ abstract material icons in CSS with an icon class
 
 
 ## Component checklist
