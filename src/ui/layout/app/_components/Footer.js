99c2fb15017ccaa93c5bdd6440fc9d1906c2f6d7
Christophe Noël <touffi51@gmail.com>
2020-06-22 20:19:40 +0200

    Update ChordMark & ChordSymbol to latest version

--- src/ui/layout/app/_components/Footer.js
+++ src/ui/layout/app/_components/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Footer() {
 	return (
 		<div className={'appFooter'}>
-			Chords Charts Studio v0.8.0 - ChordMark v0.4.0
+			Chords Charts Studio v0.8.3 - ChordMark v0.5.4 - ChordSymbol v0.5.1
 		</div>
 	);
 }
