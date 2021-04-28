eb95e8a81e330f5b533f25568419a08100367ba7
Christophe Noël <touffi51@gmail.com>
2021-04-28 18:10:50 +0200

    update dependency and fix unit tests

--- tests/unit/editor/songEditor/prosemirror/converters/stateToText.spec.js
+++ tests/unit/editor/songEditor/prosemirror/converters/stateToText.spec.js
@@ -14,18 +14,18 @@ describe('stateToText', () => {
 describe('Behaviour', () => {
 	test('Should convert state to an array of lines with plain text', () => {
 		const input =
-`<div>
-    <p>Two jumps in a week, I bet you think that's pretty clever don't you, boy?</p>
-    <p>Flying on your <em>motorcycle</em>, watching all the ground beneath you drop</p>
-    <p>You'd kill yourself for <u>recognition</u>; kill yourself to never ever stop</p>
-    <p>You broke another mirror; you're turning into something you are not</p>
-    <p></p>
-    <p><strong>Don't leave me high, don't leave me <em>dry</em></strong></p>
-    <p><strong>Don't leave me high, don't leave me <em>dry</em></strong></p>
-    <p>This line ends with a space outside all tags, and space will be lost</p> 
-    <p>This line ends with a space inside p tag </p> 
-    <p></p>
-</div>`;
+'<div>' +
+    '<p>Two jumps in a week, I bet you think that\'s pretty clever don\'t you, boy?</p>' +
+    '<p>Flying on your <em>motorcycle</em>, watching all the ground beneath you drop</p>' +
+    '<p>You\'d kill yourself for <u>recognition</u>; kill yourself to never ever stop</p>' +
+    '<p>You broke another mirror; you\'re turning into something you are not</p>' +
+    '<p></p>' +
+    '<p><strong>Don\'t leave me high, don\'t leave me <em>dry</em></strong></p>' +
+    '<p><strong>Don\'t leave me high, don\'t leave me <em>dry</em></strong></p>' +
+    '<p>This line ends with a space outside all tags, and space will be lost</p>' + 
+    '<p>This line ends with a space inside p tag </p>' + 
+    '<p></p>' +
+'</div>';
 
 		const expected = `Two jumps in a week, I bet you think that's pretty clever don't you, boy?
 Flying on your motorcycle, watching all the ground beneath you drop
