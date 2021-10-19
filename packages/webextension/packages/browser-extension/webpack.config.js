c4bddf213e94e8746c2dca6198847c3f0cd73ed8
Christophe Noël <touffi51@gmail.com>
2021-10-19 20:57:26 +0200

    improve build process

--- packages/webextension/packages/browser-extension/webpack.config.js
+++ packages/webextension/packages/browser-extension/webpack.config.js
@@ -1,6 +1,8 @@
 /* eslint-env node */
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	mode: 'production',
@@ -23,10 +25,25 @@ module.exports = {
 	},
 
 	plugins: [
+		new CleanWebpackPlugin(),
 		new CopyPlugin({
 			patterns: [{ from: 'static' }],
 		}),
 	],
+
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+				},
+			}),
+		],
+	},
 
 	module: {
 		rules: [
