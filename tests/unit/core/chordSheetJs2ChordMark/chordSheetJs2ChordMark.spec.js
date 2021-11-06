5b6112f21f8093d422e81c0ef9663b3b97458d29
Christophe Noël <touffi51@gmail.com>
2021-11-06 13:46:51 +0100

    Add chordSheetJs2ChordMark converter

--- tests/unit/core/chordSheetJs2ChordMark/chordSheetJs2ChordMark.spec.js
+++ tests/unit/core/chordSheetJs2ChordMark/chordSheetJs2ChordMark.spec.js
@@ -2,8 +2,14 @@ import fs from 'fs';
 import ChordSheetJS from 'chordsheetjs';
 
 import chordSheetJs2ChordMark from '../../../../src/core/converters/chordSheetJs2ChordMark';
+import chordProOutput1 from './chordProOutput1';
 import ultimateGuitarOutput1 from './ultimateGuitarOutput1';
 import ultimateGuitarOutput2 from './ultimateGuitarOutput2';
+
+const chordProInput1 = fs.readFileSync(
+	__dirname + '/chordProInput1.txt',
+	'utf-8'
+);
 
 const ultimateGuitarInput1 = fs.readFileSync(
 	__dirname + '/ultimateGuitarInput1.txt',
@@ -20,34 +26,31 @@ describe('chordSheetJs2ChordMark', () => {
 		expect(chordSheetJs2ChordMark).toBeInstanceOf(Function);
 	});
 
-	describe.skip.each([
-		['undefined', undefined, []],
-		['empty object', {}, []],
-		['no "lines" property', { nolines: [] }, []],
-		['"lines" property is an object', { lines: {} }, []],
-		['"lines" property is a string', { lines: 'a string' }, []],
-		['"lines" property is a number', { lines: 10 }, []],
-		['"lines" property is an empty array', { lines: [] }, []],
-	])('invalid input: %s', (title, input, expected) => {
-		test('should yield an empty array', () => {
-			const result = chordSheetJs2ChordMark(input);
-			expect(result).toEqual(expected);
+	describe('ChordSheetJs created from Ultimate Guitar source', () => {
+		test('should properly convert ChordSheetJS format', () => {
+			const parser = new ChordSheetJS.UltimateGuitarParser();
+			const parsed = parser.parse(ultimateGuitarInput1);
+
+			const result = chordSheetJs2ChordMark(parsed).split('\n');
+			expect(result).toEqual(ultimateGuitarOutput1);
+		});
+
+		test('should handle section labels', () => {
+			const parser = new ChordSheetJS.UltimateGuitarParser();
+			const parsed = parser.parse(ultimateGuitarInput2);
+
+			const result = chordSheetJs2ChordMark(parsed).split('\n');
+			expect(result).toEqual(ultimateGuitarOutput2);
 		});
 	});
 
-	test('should properly convert ChordSheetJS format', () => {
-		const parser = new ChordSheetJS.UltimateGuitarParser();
-		const parsed = parser.parse(ultimateGuitarInput1);
+	describe('ChordSheetJs created from ChordPro source', () => {
+		test('should properly convert ChordSheetJS format', () => {
+			const parser = new ChordSheetJS.ChordProParser();
+			const parsed = parser.parse(chordProInput1);
 
-		const result = chordSheetJs2ChordMark(parsed).split('\n');
-		expect(result).toEqual(ultimateGuitarOutput1);
-	});
-
-	test('should handle section labels', () => {
-		const parser = new ChordSheetJS.UltimateGuitarParser();
-		const parsed = parser.parse(ultimateGuitarInput2);
-
-		const result = chordSheetJs2ChordMark(parsed).split('\n');
-		expect(result).toEqual(ultimateGuitarOutput2);
+			const result = chordSheetJs2ChordMark(parsed).split('\n');
+			expect(result).toEqual(chordProOutput1);
+		});
 	});
 });
