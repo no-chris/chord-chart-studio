e5b1e855a6b3327aef51102e64f84facc2a0fd74
Christophe Noël <touffi51@gmail.com>
2021-11-07 23:00:34 +0100

    Use combine-section-reducers instead of combineReducers

--- src/ui/layout/reducers.js
+++ src/ui/layout/reducers.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
+import combineSectionReducers from 'combine-section-reducers';
 import app from './app/_state/reducers';
 
-export default combineReducers({
+export default combineSectionReducers({
 	app,
 });
--- src/ui/reducers.js
+++ src/ui/reducers.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
+import combineSectionReducers from 'combine-section-reducers';
 import layout from './layout/reducers';
 
-export default combineReducers({
+export default combineSectionReducers({
 	layout,
 });
