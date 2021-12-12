332f86d1c96e770c1bc58f0a8bec0a3c7f182460
Christophe Noël <touffi51@gmail.com>
2021-12-12 13:34:10 +0100

    build

--- src/core/getVersions.js
+++ src/core/getVersions.js
@@ -1,1 +1,1 @@
-export default () => ({'chord-charts-studio':'v0.8.3','chord-symbol':'v2.1.0','chord-mark':'v0.8.0-beta.1'});
+export default () => ({'chord-charts-studio':'v0.8.3','chord-symbol':'v2.1.0','chord-mark':'v0.8.0'});
