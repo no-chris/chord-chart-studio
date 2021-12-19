50ae11b79ca2839624c07db6b804ee3e88857c84
Christophe Noël <touffi51@gmail.com>
2021-12-19 20:17:42 +0100

    init documentation website

---
+++ packages/documentation/docusaurus.config.js
@@ -0,0 +1,111 @@
+/* eslint-env node */
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: 'Chord Charts Studio',
+	tagline: 'Create Chords Charts that looks like you',
+	url: 'https://chord-charts-studio.netlify.app',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'no-chris', // Usually your GitHub org/user name.
+	projectName: 'chord-charts-studio', // Usually your repo name.
+
+	plugins: ['docusaurus-plugin-sass'],
+
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/no-chris/chord-charts-studio/tree/master/packages/documentation',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.scss'),
+				},
+				gtag: {
+					trackingID: 'G-EGKBT2J600',
+					anonymizeIP: true,
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: 'Chord Charts Studio',
+				logo: {
+					alt: 'Chord Charts Studio',
+					src: 'img/icon_whitebg.png',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'overview/what-is-chordmark',
+						position: 'left',
+						label: 'User guide',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: '5 minutes tutorial',
+								to: '/docs/getting-started',
+							},
+							{
+								label: 'Licenses',
+								to: '/docs/licenses',
+							},
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Report issues',
+								href: 'https://github.com/no-chris/chord-mark/issues',
+							},
+							{
+								label: 'User forum',
+								href: 'https://github.com/no-chris/chord-mark/discussions',
+							},
+						],
+					},
+					{
+						title: 'Related tools',
+						items: [
+							{
+								label: 'ChordSymbol',
+								href: 'https://chord-symbol.netlify.app',
+							},
+							{
+								label: 'ChordMark',
+								href: 'https://chordmark.netlify.app',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Christophe Noël. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+};
+
+module.exports = config;
---
+++ packages/documentation/sidebars.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+	// By default, Docusaurus generates a sidebar from the docs folder structure
+	docs: [
+		'chord-charts-studio/overview',
+		'chord-charts-studio/import',
+		'chord-charts-studio/faq',
+	],
+};
+
+module.exports = sidebars;
