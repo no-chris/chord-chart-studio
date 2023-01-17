2f850856d6a900a5f53161663b0b045b14f94836
Christophe Noël <touffi51@gmail.com>
2023-01-17 23:55:53 +0100

    build & trigger deploy preview

--- packages/chord-chart-studio/src/core/getVersions.js
+++ packages/chord-chart-studio/src/core/getVersions.js
@@ -1,1 +1,1 @@
-export default () => ({'chord-chart-studio':'v0.13.0','chord-symbol':'3.0.0','chord-mark':'0.13.0-beta.0'});
+export default () => ({'chord-chart-studio':'v0.13.0','chord-symbol':'3.0.0','chord-mark':'0.13.0-beta.1'});
