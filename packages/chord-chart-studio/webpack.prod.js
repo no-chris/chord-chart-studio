cb99cc5c7b797afa5167e35f0370efcf675f48b9
Christophe Noël <touffi51@gmail.com>
2024-01-31 20:22:20 +0100

    cache google font css

--- packages/chord-chart-studio/webpack.prod.js
+++ packages/chord-chart-studio/webpack.prod.js
@@ -56,11 +56,27 @@ module.exports = merge(common, {
 			clientsClaim: true,
 			skipWaiting: true,
 			runtimeCaching: [
+				// based on https://developer.chrome.com/docs/workbox/modules/workbox-recipes
+				// and https://stackoverflow.com/questions/52451678/caching-google-fonts-using-workbox
+				{
+					urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+					handler: 'StaleWhileRevalidate',
+					options: {
+						cacheName: 'google-fonts-stylesheets',
+					},
+				},
 				{
 					urlPattern: /^https:\/\/fonts\.gstatic\.com/,
 					handler: 'StaleWhileRevalidate',
 					options: {
 						cacheName: 'google-fonts-webfonts',
+						cacheableResponse: {
+							statuses: [0, 200],
+						},
+						expiration: {
+							maxAgeSeconds: 60 * 60 * 24 * 365,
+							maxEntries: 30,
+						},
 					},
 				},
 			],
