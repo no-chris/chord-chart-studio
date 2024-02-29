07d7c4a13778d43fb8138bb09c9d26a24e8245b1
Christophe Noël <touffi51@gmail.com>
2024-02-29 22:49:12 +0100

    build

--- packages/webextension/build/chordChartsStudio.js
+++ packages/webextension/build/chordChartsStudio.js
@@ -1,1 +1,1 @@
-chrome.runtime.onMessage.addListener((function(e,n,o){return setTimeout((()=>window.postMessage(e)),1e3),o("ok"),!0}));
+"use strict";const n=function(e,o,s){return setTimeout(()=>window.postMessage(e),1e3),s("ok"),!0};chrome.runtime.onMessage.addListener(n);
