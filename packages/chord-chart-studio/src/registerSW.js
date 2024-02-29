a0e2a100bd8ecc77d83fa29f0aabd967f95b8532
Christophe Noël <touffi51@gmail.com>
2024-02-29 21:32:49 +0100

    remove old service worker

--- packages/chord-chart-studio/src/registerSW.js
+++
@@ -1,9 +0,0 @@
-const serviceWorkerPath = '/service-worker.js';
-
-export default function registerSW() {
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker
-			.register(serviceWorkerPath)
-			.catch((err) => console.log('Cannot register service worker', err));
-	}
-}
