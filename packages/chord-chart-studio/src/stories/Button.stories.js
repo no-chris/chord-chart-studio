b8fc8677354352bbad977a750c9a31bb8c4595cf
Christophe Noël <touffi51@gmail.com>
2024-03-02 20:49:25 +0100

    formatting fix

--- packages/chord-chart-studio/src/stories/Button.stories.js
+++ packages/chord-chart-studio/src/stories/Button.stories.js
@@ -15,7 +15,8 @@ export default {
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
-	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked:
+	//https://storybook.js.org/docs/essentials/actions#action-args
 	args: { onClick: fn() },
 };
 
