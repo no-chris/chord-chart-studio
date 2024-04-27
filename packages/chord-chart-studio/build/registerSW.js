ec24625217e16196afd54b664a9a2daa0c0cc6a9
Christophe Noël <touffi51@gmail.com>
2024-04-27 22:45:05 +0200

    fix icons build

--- packages/chord-chart-studio/build/registerSW.js
+++ packages/chord-chart-studio/build/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app/sw.js', { scope: '/app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
